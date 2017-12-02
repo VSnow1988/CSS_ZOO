934179d27bba4183ad6f61ffd7f0128aeab35704
Vincent Snow <vincentsnow@gmx.com>
2017-12-01 20:09:28 -0800

    Not quite

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ $(document).ready(function(){
 	//mousedown and mouseup moves the buttons
 	
 	$('#donotpush').on('click',function(){ 
-		$('.cage').css("border", "10px solid transparent");
-		//Play the roar sound, wait 5 seconds.
+		$('.cage').css("border", "10px solid transparent").delay(3000);
+		$('#roara').play();
+		
 		$('.octopus').hide();
 		$('.cage').hide();
 		$('#donotpush').hide();
-		$('body').css("background-color","red");
+		$('#scratch').show();
 		$('#reset').show();
 	});
 	
 	$('#reset').on('click',function(){ 
 		$('#reset').hide();
-		$('body').css("background-color","#888");
+		$('#scratch').hide();
 		$('.cage').css("border", "10px solid #555");
 		$('.octopus').show();
+		$('.octopus').css("background-color","");
+		$('.bat').css("opacity","");
 		$('.cage').show();
 		$('#donotpush').show();
+		$('.elephant').css({"background-color":"", "border":"", "height":"", "width":""});
 	});
 	
 	
@@ -30,6 +34,24 @@ $(document).ready(function(){
 		$('#oleftarm').css('background-color',rcolor);
 		$('#oleftbackarm').css('background-color',rcolor);
 		$('#orightbackarm').css('background-color',rcolor);
+	});
+	
+	$('.bat').on('click', function(){
+		$('.bat').animate({opacity: 0},5000);
+	});
+	
+	$('.elephant').on('click', function(){
+		let bigger = (((parseInt($('#ebody').height())) + ((parseInt($('#ebody').height()))*0.05)).toString())+"px";
+		let fatter = (((parseInt($('#ebody').width())) + ((parseInt($('#ebody').width()))*0.05)).toString())+"px";
+		console.log(bigger);
+		console.log(fatter);
+		if (parseInt(bigger) < 132.3){
+		$('#ebody').css("height", bigger);
+		$('#ebody').css("width", fatter);
+		}
+		else{
+		$('.elephant').css({"background-color":"transparent", "border":"none"});	
+		}
 	});
 	
 	
